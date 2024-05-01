a6a5559c17175633da0f033b146d127adb942214
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-01 13:05:52 -0500

    updated sandbox.js with functions and methods

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -19,3 +19,8 @@ console.log(fullName.length);
 
 //string methods
 console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
