7e6a049358546115ef1c09d68e2a4dc1e7485d50
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-01 12:01:50 -0500

    updated sandbox.js with functions and methods

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,17 +1,21 @@
-let myAge = 25;       //basic variable, no spaces in a variable name. can't start with a number
-let year = 2019;        //reserved words: const, let, var, etc
+//strings
+console.log('hello, world');
 
-console.log(age, year);
+let email = "email@email.com";
+console.log(email);
 
-age = 30;
-console.log(age);
+//string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+let fullName = firstName + ' ' + lastName;
 
-const points = 100;
-// points = 50;         can't do this bc the const variable makes it constant
-console.log(points);
+console.log(fullName);
 
-var score = 75;     //older way to create variables
-console.log(score);
+//getting characters
+console.log(fullName[0]);
 
-/* when using newer variables (let/const), they might not always work in 100% of browsers
-    older browsers, like IE11, don't support the modern features */
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
