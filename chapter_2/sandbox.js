314b05ae52af41fc48c1af75bb77ce96d5474dc2
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-01 13:55:39 -0500

    updated sandbox.js with booleans and comparisons

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,3 +1,30 @@
-let age = null; //null is an intentional value, whereas NaN is just undefined
+//booleans & comparisons
+console.log(true, false, "true", "false");
 
-console.log(age, age+3, `the age is ${age}`);
+//methods can return booleans
+let email = 'luigi@thenetninja.co.uk';
+let names = ['mario', 'luigi', 'toad'];
+
+// let result = email.includes('!');
+// let result = names.includes('bowser');
+
+// console.log(result);
+
+//comparison operators
+let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+let name = 'shaun';
+
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal'); //later letters in the alphabet are greater than earlier letters
+console.log(name > 'Shaun'); //lowercase letters are greater than uppercase letters
+console.log(name > 'Crystal'); //any lowercase is greater than any uppercase letter
