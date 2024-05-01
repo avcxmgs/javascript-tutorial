7297263185407b66b85a1069774aa3b03e8b477d
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-01 11:40:30 -0500

    updated sandbox.js with usage of variables

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,2 +1,11 @@
-console.log(1);
-console.log(2);
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+// points = 50;         can't do this bc the const variable makes it constant
+console.log(points);
