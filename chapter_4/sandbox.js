b026f1f1421a1ae0e4dc986a010f40483d01d4a2
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-02 09:36:56 -0500

    updated sandbox.js with arrow functions

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,13 +1,10 @@
-//returning values
+//regular function
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
 
-// const speak = function(name = 'luigi', time = 'night'){
-//     console.log(`good ${time} ${name}`);
-// };
-
-
-const calcArea = function(radius){
-    return 3.14 * radius**2;
-}
+//arrow function
+const calcArea = radius => 3.14 * radius**2;
 
 const area = calcArea(5);
-console.log(area);
+console.log('area is: ', area);
