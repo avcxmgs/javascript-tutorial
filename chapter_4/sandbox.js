a26fa0af1d31eb61b02623e50137d9d37e0c884a
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-02 09:43:34 -0500

    updated sandbox.js with more arrow function practice

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,10 +1,27 @@
-//regular function
-// const calcArea = function(radius){
-//     return 3.14 * radius**2;
+//practice arrow function
+
+// const greet = function(){
+//     return 'hello, world';
+// };
+
+// const greet = () => 'hello, world';
+// console.log(greet());
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
 // }
 
-//arrow function
-const calcArea = radius => 3.14 * radius**2;
 
-const area = calcArea(5);
-console.log('area is: ', area);
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+
+console.log(bill([10,15,30], 0.2))
