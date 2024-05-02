9b6ed32152069e10fced83feed4a13a2a1644004
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-02 09:24:55 -0500

    created sandbox.js with function expressions and declarations

---
+++ chapter_4/sandbox.js
@@ -0,0 +1,18 @@
+greet();
+greet();
+greet();
+
+speak();
+speak();
+speak();
+
+//function declaration
+function greet(){
+    console.log('hello there');
+}
+
+
+//function expression
+const speak = function(){
+    console.log('good day!');
+};
