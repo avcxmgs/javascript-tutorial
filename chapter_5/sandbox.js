b46ceb03183664d48029f9a87d826eb9f5bd0dc7
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-02 10:39:53 -0500

    updated sandbox.js with more functions in objects

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -6,15 +6,21 @@ let user = {
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
     blogs: ['why mac & cheese rules', '10 things to make with marmite'],
-    login: function(){
+    login(){
         console.log('the user logged in');
     },
-    logout: function(){
+    logout(){
         console.log('the user logged out');
+    },
+    logBlogs(){
+        //console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        })
     }
 };
 
-user.login();
+user.logBlogs();
 
-const name = 'mario';
-name.toUpperCase();
+console.log(this);
